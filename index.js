122d8a3bfb667a71985ed0dc56850f36b278f4bc
Danny Guo <dannyguo91@gmail.com>
2017-09-06 12:36:50 -0500

    Pull out the CSS and JS into separate files

---
+++ index.js
@@ -0,0 +1,31 @@
+anchors.options.placement = 'left';
+// One of the longer FAQ questions doesn't work with the default
+// truncate value. The anchor link looks fine, but Chrome doesn't
+// jump to it when loading in a new tab.
+anchors.options.truncate = 50;
+anchors.add();
+
+var md = new Remarkable({
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(lang, str).value;
+            } catch (err) {}
+        }
+
+        try {
+            return hljs.highlightAuto(str).value;
+        } catch (err) {}
+
+        return ''; // use external default escaping
+    }
+});
+
+var rendered = document.querySelector('#rendered');
+rendered.innerHTML = md.render(document.querySelector('textarea').textContent);
+
+document.querySelector('textarea').addEventListener('keyup', function (e) {
+    rendered.innerHTML = md.render(e.target.value);
+});
+
+autosize(document.querySelector('textarea'));
