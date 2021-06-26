2634a2573af7890b420f6481090d3b9730253723
Danny Guo <dannyguo91@gmail.com>
2021-06-26 15:30:36 -0400

    Upgrade Prettier

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-anchors.options.placement = 'left';
+anchors.options.placement = "left";
 // One of the longer FAQ questions doesn't work with the default
 // truncate value. The anchor link looks fine, but Chrome doesn't
 // jump to it when loading in a new tab.
@@ -6,7 +6,7 @@ anchors.options.truncate = 50;
 anchors.add();
 
 var md = new Remarkable({
-    highlight: function(str, lang) {
+    highlight: function (str, lang) {
         if (lang && hljs.getLanguage(lang)) {
             try {
                 return hljs.highlight(lang, str).value;
@@ -17,15 +17,15 @@ var md = new Remarkable({
             return hljs.highlightAuto(str).value;
         } catch (err) {}
 
-        return ''; // use external default escaping
+        return ""; // use external default escaping
     }
 });
 
-var rendered = document.querySelector('#rendered');
-rendered.innerHTML = md.render(document.querySelector('textarea').textContent);
+var rendered = document.querySelector("#rendered");
+rendered.innerHTML = md.render(document.querySelector("textarea").textContent);
 
-document.querySelector('textarea').addEventListener('keyup', function(e) {
+document.querySelector("textarea").addEventListener("keyup", function (e) {
     rendered.innerHTML = md.render(e.target.value);
 });
 
-autosize(document.querySelector('textarea'));
+autosize(document.querySelector("textarea"));
