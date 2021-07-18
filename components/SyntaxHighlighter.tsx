90fc44bf33372c3e61b04718ef72877798939c44
Danny Guo <dannyguo91@gmail.com>
2021-07-17 18:56:33 -0700

    Use LightAsync

--- components/SyntaxHighlighter.tsx
+++ components/SyntaxHighlighter.tsx
@@ -1,15 +1,15 @@
-import {Light} from "react-syntax-highlighter";
+import {LightAsync} from "react-syntax-highlighter";
 import {a11yDark} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 import python from "react-syntax-highlighter/dist/cjs/languages/hljs/python";
 import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
 
-Light.registerLanguage("python", python);
-Light.registerLanguage("javascript", javascript);
+// LightAsync.registerLanguage("python", python);
+// Light.registerLanguage("javascript", javascript);
 
 function SyntaxHighlighter(props) {
     return (
-        <Light
+        <LightAsync
             language={props.language}
             style={a11yDark}
             children={props.value || ""}
