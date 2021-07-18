9ef0be79fa6d71a3b1b19168255ae26dc58745a6
Danny Guo <dannyguo91@gmail.com>
2021-07-17 18:49:40 -0700

    register js

--- components/SyntaxHighlighter.tsx
+++ components/SyntaxHighlighter.tsx
@@ -1,16 +1,17 @@
 import {Light} from "react-syntax-highlighter";
-// import {a11yLight} from "react-syntax-highlighter/dist/cjs/styles/hljs";
-import {gruvboxDark} from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import {a11yDark} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 import python from "react-syntax-highlighter/dist/cjs/languages/hljs/python";
+import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
 
 Light.registerLanguage("python", python);
+Light.registerLanguage("javascript", javascript);
 
 function SyntaxHighlighter(props) {
     return (
         <Light
             language={props.language}
-            style={gruvboxDark}
+            style={a11yDark}
             children={props.value || ""}
         />
     );
