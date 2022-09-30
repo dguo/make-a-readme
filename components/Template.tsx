c7d1f3d3a4c53892ffcd0615a798576c80782090
Maciej Strzelecki <mstrzele@users.noreply.github.com>
2022-10-01 00:01:06 +0200

    fix MD013
    MD013 - Line length
    https://github.com/DavidAnson/markdownlint/blob/v0.26.2/doc/Rules.md#md013

--- components/Template.tsx
+++ components/Template.tsx
@@ -32,7 +32,8 @@ foobar.singularize('phenomena')
 
 ## Contributing
 
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
 
