f73185965d242123f95a499e8d005bbd6987c91e
Maciej Strzelecki <mstrzele@users.noreply.github.com>
2022-09-30 23:58:45 +0200

    fix violation of MD022
    MD022 - Headings should be surrounded by blank lines
    https://github.com/DavidAnson/markdownlint/blob/v0.26.2/doc/Rules.md#md022

--- components/Template.tsx
+++ components/Template.tsx
@@ -31,11 +31,13 @@ foobar.singularize('phenomena')
 \`\`\`
 
 ## Contributing
+
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)`;
 
 export function Template() {
