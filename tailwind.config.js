463d484b38869d9e2f73e49a9de1ae77bb2549dd
Danny Guo <dannyguo91@gmail.com>
2022-11-08 23:36:52 -0500

    Upgrade Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-    mode: "jit",
-    purge: [
+    content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}"
     ],
-    darkMode: false,
     theme: {
         extend: {
             colors: {
