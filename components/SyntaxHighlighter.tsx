c8264b9c89bc1efe8214b4a546dd9352be026cda
Danny Guo <dannyguo91@gmail.com>
2024-02-27 11:16:23 -0500

    Handle inline code elements

--- components/SyntaxHighlighter.tsx
+++ components/SyntaxHighlighter.tsx
@@ -10,13 +10,29 @@ function SyntaxHighlighter(props) {
     // An example className is "language-python"
     const languageMatch = /language-(\w+)/.exec(className || "");
 
-    return (
+    /* This is a simplified version of the example from the react-markdown docs:
+       https://github.com/remarkjs/react-markdown?tab=readme-ov-file#use-custom-components-syntax-highlight
+
+       If we have a language, we render it as a code block with syntax
+       highlighting. Otherwise, we render an inline code element.
+
+       One issue is that this approach doesn't handle a code block that doesn't
+       have a language identifier, though GitHub supports that:
+       https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/creating-and-highlighting-code-blocks#fenced-code-blocks
+
+       It is rendered as an inline code element instead of a plain text code
+       block. See this issue for more context:
+       https://github.com/remarkjs/react-markdown/issues/776
+    */
+    return languageMatch ? (
         <LightAsync
             PreTag="div"
             children={children}
-            language={languageMatch?.[1] ?? "txt"}
+            language={languageMatch[1]}
             style={a11yDark}
         />
+    ) : (
+        <code className={className} children={children} />
     );
 }
 
