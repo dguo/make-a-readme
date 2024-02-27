6d9e22931e3bb0c6b926a7bb396d371f78e6e719
Danny Guo <dannyguo91@gmail.com>
2024-02-27 11:46:10 -0500

    Upgrade dependencies (#84)
    Keep Next.js on 14.0.0 instead of 14.1.0 because of a dev
    server crashing issue:
    https://github.com/vercel/next.js/issues/61905

--- components/SyntaxHighlighter.tsx
+++ components/SyntaxHighlighter.tsx
@@ -2,12 +2,44 @@ import {LightAsync} from "react-syntax-highlighter";
 import {a11yDark} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 function SyntaxHighlighter(props) {
-    return (
+    const {children, className} = props;
+    if (!children) {
+        return null;
+    }
+
+    // An example className is "language-python"
+    const languageMatch = /language-(\w+)/.exec(className || "");
+
+    /* This is a simplified version of the example from the react-markdown docs:
+       https://github.com/remarkjs/react-markdown?tab=readme-ov-file#use-custom-components-syntax-highlight
+
+       If we have a language, we render it as a code block with syntax
+       highlighting. Otherwise, we render an inline code element.
+
+       One issue is that this approach doesn't handle a code block that doesn't
+       have a language identifier, though GitHub supports that:
+       https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/creating-and-highlighting-code-blocks#fenced-code-blocks
+
+       It is rendered as an inline code element instead of a code block. See
+       this issue for more context:
+       https://github.com/remarkjs/react-markdown/issues/776
+
+       If we could distinguish between inline code and code blocks, we could
+       still use the syntax highlighter and pass in plain text for the language.
+       Instead, our hacky fix for this is to apply CSS by targeting the
+       language-less code blocks and copying over the styling that is provided
+       by github-markdown-css. See the ".markdown-body pre > code" selector in
+       global.css.
+    */
+    return languageMatch ? (
         <LightAsync
-            language={props.language}
+            PreTag="div"
+            children={children}
+            language={languageMatch[1]}
             style={a11yDark}
-            children={props.value || ""}
         />
+    ) : (
+        <code className={className} children={children} />
     );
 }
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    distDir: "build",
+    output: "export",
     productionBrowserSourceMaps: true
 };
+
+module.exports = nextConfig;
